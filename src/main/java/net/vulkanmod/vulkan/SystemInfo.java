244ada3f06671852696e863f2ac3e926dce4a08b
HitomiLovely <hitomilovely8@gmail.com>
2025-05-29 11:03:57 +0800

    feat: add instance version + simple android fixes

--- src/main/java/net/vulkanmod/vulkan/SystemInfo.java
+++ src/main/java/net/vulkanmod/vulkan/SystemInfo.java
@@ -1,12 +1,58 @@
 package net.vulkanmod.vulkan;
 
+import net.vulkanmod.Initializer;
 import oshi.hardware.CentralProcessor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
 
 public class SystemInfo {
     public static final String cpuInfo;
 
     static {
+        cpuInfo = isRunningOnAndroid() ? getProcessorNameForAndroid() : getProcessorNameForDesktop();
+    }
+
+    public static String getProcessorNameForAndroid() {
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/cpuinfo"))) {
+            // Attempt to find processor name from "Hardware", "model name", or "Processor" fields
+            String processorName = br.lines()
+                    .map(String::trim)
+                    .filter(line -> line.startsWith("Hardware") || line.startsWith("model name") || line.startsWith("Processor"))
+                    .map(line -> {
+                        String[] parts = line.split(":\\s+", 2);
+                        if (parts.length == 2) {
+                            return parts[1].trim() + " (SoC)";
+                        }
+                        return null;
+                    })
+                    .filter(Objects::nonNull)
+                    .findFirst()
+                    .orElse("Unknown");
+
+            if ("Unknown".equals(processorName)) {
+                Initializer.LOGGER.warn("Unable to determine processor name");
+            }
+
+            return processorName;
+        } catch (IOException e) {
+            Initializer.LOGGER.error("Error reading /proc/cpuinfo", e);
+            return "Unknown";
+        }
+    }
+
+    public static String getProcessorNameForDesktop() {
         CentralProcessor centralProcessor = new oshi.SystemInfo().getHardware().getProcessor();
-        cpuInfo = String.format("%s", centralProcessor.getProcessorIdentifier().getName()).replaceAll("\\s+", " ");
+        return centralProcessor.getProcessorIdentifier().getName().replaceAll("\\s+", " ");
+    }
+
+    public static boolean isRunningOnAndroid() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return (osName.contains("linux") || osName.contains("android")) && (System.getenv("POJAV_ENVIRON") != null ||
+                System.getenv("SCL_ENVIRON") != null ||
+                System.getenv("POJAV_RENDERER") != null ||
+                System.getenv("MOJO_RENDERER") != null);
     }
 }
